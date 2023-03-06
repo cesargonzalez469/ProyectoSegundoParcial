0c306831743bd1a15910f738d1ad6bd747e9834e
GustavoAngel2 <124698903+GustavoAngel2@users.noreply.github.com>
2023-03-06 08:12:55 -0600

    Create BDContext.cs

---
+++ PSPSDO/BD/BDContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSPSDO.BD
+{
+    internal class Class1
+    {
+    }
+}
