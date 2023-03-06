0271979df37167adaced05e38f8f9f897aeee8c1
GustavoAngel2 <124698903+GustavoAngel2@users.noreply.github.com>
2023-03-06 09:13:54 -0600

    Update frmCarreras.cs

--- PSPSDO/Forms/frmCarreras.cs
+++ PSPSDO/Forms/frmCarreras.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PSPSDO.BD;
 
 namespace PSPSDO.Forms
 {
@@ -15,6 +18,16 @@ namespace PSPSDO.Forms
         public frmCarreras()
         {
             InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            ArrayList parametros = new ArrayList();
+            BDContext bd = new BDContext();
+            parametros.Add(new SqlParameter { ParameterName = "@pId", SqlDbType = System.Data.SqlDbType.VarChar, Value = 1 });
+            DataSet ds = bd.Fill("sp_GetUsuarios", parametros);
+            dgvCarreras.DataSource = ds.Tables[0];
+            
         }
     }
 }
